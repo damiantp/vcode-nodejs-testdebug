320baa2c486f342681e45e0052d00866836a49bc
amtdamian <damian.tirado@appmediatechnology.com>
2016-06-16 10:42:59 +0200

    generate .js and .map with new text in console.log

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var s = http.createServer(function (req, res) {
     // comment
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-    console.log("1");
+    console.log("Testing TS remote debugging"); //  <<--- set breakpoint here (will move to previous line)
 });
 s.listen(8000);
 console.log('Server running!');
